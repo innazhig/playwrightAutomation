38a924cfa67080460a60cb30a95b8741ae39efa5
innazhig <128564141+innazhig@users.noreply.github.com>
2024-05-30 21:45:02 -0500

    AUthentication with setup project (#9)

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASEURL || "http://127.0.0.1:3000",
+    //baseURL: process.env.BASEURL || "http://127.0.0.1:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -37,14 +37,24 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    // Setup project
+    { name: "setup", testMatch: /.*\.setup\.ts/ },
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], headless: false },
+      use: {
+        ...devices["Desktop Chrome"],
+        headless: false,
+        // Use prepared auth state.
+        storageState: "./.auth/user.json",
+      },
+      dependencies: ["setup"],
     },
     /*
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'], storageState: './.auth/user.json',
+      },
+      dependencies: ['setup'],
     },
 
     {
